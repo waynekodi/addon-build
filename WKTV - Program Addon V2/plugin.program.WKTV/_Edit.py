7dcf7d64563d62c51de93b868615b1b2665a2c65
waynekodi <whitworth.kodi.tv@gmail.com>
2016-09-21 15:00:03 +0100

    Update

--- WKTV - Program Addon V2/plugin.program.WKTV/_Edit.py
+++ WKTV - Program Addon V2/plugin.program.WKTV/_Edit.py
@@ -1,4 +1,4 @@
 import xbmcaddon
 
-MainBase = 'http://goo.gl/oNkF0a'
+MainBase = 'https://raw.githubusercontent.com/waynekodi/addon-build/master/WKTV%20-%20Program%20Addon%20V2/Example/home.txt'
 addon = xbmcaddon.Addon('plugin.program.WKTV')
